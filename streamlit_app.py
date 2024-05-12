77a6a19deecead71db141e7f0099a62357e38bfd
AI Research <62849653+pacnimo@users.noreply.github.com>
2024-05-12 13:32:44 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 from moviepy.editor import VideoFileClip, TextClip, CompositeVideoClip
 import os
-os.environ['SERVER_ENABLECORS'] = 'false'
-os.environ['SERVER_ENABLEXSRFPROTECTION'] = 'false'
 import sys
 from contextlib import redirect_stdout
 import io
